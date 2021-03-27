f2520cb198b41c686620502488e581fc79e44393
ahnjongmin <79144650+ahnjongmin@users.noreply.github.com>
2021-03-27 18:03:45 +0900

    style: Adapting code to last meeting's conclusion

--- src/main/java/xxx/yyy/App.java
+++ src/main/java/xxx/yyy/App.java
@@ -6,18 +6,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-
 public class App
 {
 
+    public void Parse_Input(){
+
+    }
 
 
-
-
-
-
-
-    public static void main(String[] args){
+    public void Load_Data(){
         try{
             //파일 객체 생성
             File file = new File("C:\\Users\\ajm69\\Desktop\\maven_test\\maven_test\\data\\movies.dat");
@@ -32,10 +29,38 @@ public class App
             //.readLine()은 끝에 개행문자를 읽지 않는다.
             bufReader.close();
         }catch (FileNotFoundException e) {
-            // TODO: handle exception
+            System.out.println("There is no file in data");
         }catch(IOException e){
             System.out.println(e);
         }
+
+    }
+
+
+    public void Parse_Data(){
+
+
+
+    }
+
+    public void Average(){
+
+
+
+    }
+
+
+
+    public void Get_Averaged_Data(){
+
+
+
+
+    }
+
+
+    public static void main(String[] args){
+
     }
 
 }

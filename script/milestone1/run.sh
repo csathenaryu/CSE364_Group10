33c616357aa5f1c954aecdb91972bdf2ef140558
ahnjongmin <79144650+ahnjongmin@users.noreply.github.com>
2021-03-27 21:53:17 +0900

    docs: Adding run.sh and dockerfile

---
+++ script/milestone1/run.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/csathenaryu/CSE364_Group10.git
+cd root/project
+mvn install
+java -cp target/cse364-project-1.0-SNAPSHOT-jar-with-dependencies.jar xxx.yyy.YourClass Adventure educator

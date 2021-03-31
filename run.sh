7a3d81c3a3af00e9db80d1d408782683b9a9ca08
hyejinkim99 <hyejinkimm99@unist.ac.kr>
2021-03-31 17:02:41 +0900

    feat : Revise run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
-git clone https://github.com/csathenaryu/CSE364_Group10.git
-git pull origin mergeFile
-cd CSE364_Group10
+rm -r testDir
+mkdir testDir
+cd testDir
+git init
+git remote add origin https://github.com/csathenaryu/CSE364_Group10.git
+git clone origin
+git pull origin develop
 mvn install
-java -cp target/CSE364-project-1.0-SNAPSHOT.jar main.java.Main adventure educator
+java -cp target/CSE364-project-1.0-SNAPSHOT.jar main.java.Main "adventure|romance" educator

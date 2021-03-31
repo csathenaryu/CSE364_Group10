91f78d9a77facee06cbbabe1333cdfc19bb65377
ahnjongmin <79144650+ahnjongmin@users.noreply.github.com>
2021-03-31 23:59:32 +0900

    feat: Change returnTrueOrFalse(I'm sorry)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -249,19 +249,27 @@ class Function {
         return a;
     }
 
-    public boolean returnTrueOrFalse(String[] line, String[] targetsProp, int targetIndex)
-    {
+    public boolean returnTrueOrFalse(String[] line, String[] targetsProp, int targetIndex) {
 
-        if(targetsProp.length == 0)
+        if (targetsProp.length == 0)
             return false;
 
         for (String str : targetsProp) {
             String a = line[targetIndex];
             a = a.toLowerCase();
-            if(!a.contains(str))
-            {
-                return false;
+
+            String[] parse_line = parseByDelimiter(a, "\\|");
+            int flag = 0;
+            for (String str2 : parse_line) {
+
+                if (str.equals(str2)) {
+                    flag++;
+                }
+
             }
+            if (flag == 0)
+                return false;
+
         }
         return true;
     }

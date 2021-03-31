dbf6aa16ca142dd28f43ad76b0b5d76c1dadad7a
hyejinkim99 <hyejinkimm99@unist.ac.kr>
2021-03-31 16:23:14 +0900

    feat : Add run.sh (pull mergeFile branch)

---
+++ run.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/csathenaryu/CSE364_Group10.git
+git pull origin mergeFile
+cd CSE364_Group10
+mvn install
+java -cp target/CSE364-project-1.0-SNAPSHOT.jar main.java.Main adventure educator

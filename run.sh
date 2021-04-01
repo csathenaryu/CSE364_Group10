726c1f7d4fa732f88c90286ba45a77a9413a61a5
ahnjongmin <79144650+ahnjongmin@users.noreply.github.com>
2021-04-01 17:54:55 +0900

    feat: change run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ git remote add origin https://github.com/csathenaryu/CSE364_Group10.git
 git clone origin
 git pull origin develop
 mvn install
-java -cp target/CSE364-project-1.0-SNAPSHOT.jar main.java.Main "adventure|romance" educator
+mvn assembly:assembly
+java -cp target/CSE364-project-1.0-SNAPSHOT-jar-with-dependencies.jar main.java.Main "adventure|romance" educator

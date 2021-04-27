e809ae5e7a433a15ec6f9a6b8c178b340a452f41
happyhddey <happyhddey>
2021-04-27 17:27:50 +0900

    feat : Add the class ArgumentParsingTest.java. Need to be revised

---
+++ src/main/java/main/java/ArgumentParsingTest.java
@@ -0,0 +1,59 @@
+package main.java;
+
+import java.util.Arrays;
+import vimprojector.parsinginputargs.*;
+
+public class ArgumentParsingTest {
+    public static void main(String[] args) {
+
+        String[] genres;
+        String[] occupation;
+        String[] age;
+        String[] gender;
+
+        String[] aaa = new String[4];
+        aaa[3] = "\"Animation|Drama|hardware\"";
+        aaa[2] = "\"Retired\"";
+        aaa[1] = "\"36\"";
+        aaa[0] = "\"M\"";
+
+
+/*
+        // Args[0]: genders
+        try{
+            if(aaa[0] == "\"\"")
+                throw new Exception();
+            gender = ArgsPreprocessing.preprocess(Gender.list, aaa[0], "\\|", new Gender());
+        } catch (Exception e){
+            gender = Gender.list.getAll().toArray(new String[0]);
+        }
+        // Args[1]: ages
+        try{
+            age = ArgsPreprocessing.preprocess(Age.list, aaa[1], "\\|", new Age());
+        } catch (Exception e){
+            age = Age.list.getAll().toArray(new String[0]);
+        }
+        // Args[2]: occupation
+        try{
+            if(aaa[2] == "\"\"")
+                throw new Exception();
+            occupation = ArgsPreprocessing.preprocess(Occupation.list, aaa[2], "\\|", new Occupation());
+        } catch (Exception e){
+            occupation = Occupation.list.getAll().toArray(new String[0]);
+        }
+        // Args[3]: movie genres
+        try{
+            if(aaa[3] == "\"\"")
+                throw new Exception();
+            genres = ArgsPreprocessing.preprocess(MovieGenres.list, aaa[3], "\\|", new MovieGenres());
+        } catch (Exception e){
+            genres = MovieGenres.list.getAll().toArray(new String[0]);
+        }
+
+        System.out.println("Gender : " + Arrays.toString(gender));
+        System.out.println("Age : " + Arrays.toString(age));
+        System.out.println("Genres : "+ Arrays.toString(genres));
+        System.out.println("Occupation Code : " + Arrays.toString(occupation));
+        System.out.println();*/
+    }
+}

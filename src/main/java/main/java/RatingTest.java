988f84dcb877d1dc81313c2fc71100f2505395f1
happyhddey <happyhddey>
2021-04-28 00:52:04 +0900

    refactor : Remove the class TotalRating because it is seperated from RatingTest.java

--- src/main/java/main/java/RatingTest.java
+++ src/main/java/main/java/RatingTest.java
@@ -7,7 +7,7 @@ import java.util.Map.Entry;
 
 import vimprojector.customdatastructure.*;
 import vimprojector.loadingdata.*;
-import vimprojector.recommender.RatingCounter;
+import vimprojector.recommender.*;
 
 
 public class RatingTest {
@@ -128,35 +128,3 @@ class GetTopRating {
 
 }
 
-
-class TotalRating{
-
-    RatingCounter ratingCounter;
-    ArrayList<HashMap<String, String>> ratingData;
-    Bitmap targetMovie;
-    Bitmap targetUser;
-
-    public TotalRating(ArrayList<HashMap<String, String>> ratData, Bitmap targetMovieList, Bitmap targetUserList){
-        ratingCounter = new RatingCounter();
-        ratingData = ratData;
-        targetMovie = targetMovieList;
-        targetUser = targetUserList;
-    }
-
-    public float getTotalRating() {
-
-        for (HashMap<String, String> rat : ratingData){
-            int userID = Integer.parseInt(rat.get("UserID"));
-            int movieID = Integer.parseInt(rat.get("MovieID"));
-            int rating = Integer.parseInt(rat.get("Rating"));
-            updateRating(movieID, userID, rating);
-        }
-        return ratingCounter.getAverageRating();
-    }
-
-    public void updateRating(int movieID, int userID, int rating) {
-        if (targetMovie.getAt(movieID) && targetUser.getAt(userID)) {
-            ratingCounter.update(rating);
-        }
-    }
-}

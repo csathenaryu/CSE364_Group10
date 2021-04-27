8af03251a9de4999e50ea60750731c92237f431e
happyhddey <happyhddey>
2021-04-27 21:36:25 +0900

    feat : Add the class ParsingGenres

---
+++ src/main/java/vimprojector/parsinginputargs/ParsingGenres.java
@@ -0,0 +1,23 @@
+package vimprojector.parsinginputargs;
+
+import vimprojector.customdatastructure.Pair;
+import java.util.ArrayList;
+
+public class ParsingGenres extends ParsingInputArgument{
+    static Pair propertyMapping = new Pair(
+            "action:action,animation:animation,children's:children's,comedy:comedy,crime:crime,documentary:documentary,drama:drama,fantasy:fantasy,film-noir:film-noir,horror:horror,musical:musical,mystery:mystery,romance:romance,scifi:scifi,thriller:thriller,war:war,western:western");
+
+    public String[] parseProperty(String inputProp, String delimiter){
+        return getParsedProperty(propertyMapping, inputProp, delimiter);
+    }
+
+    @Override
+    public void addProperty(ArrayList<String> propertyList, String property){
+        if (propertyMapping.has(property) && (!propertyList.contains(property))){
+            propertyList.add(propertyMapping.get(property));
+        }
+        else {
+            System.out.println(property + " is invalid or already exist. Try using another genres.");
+        }
+    }
+}

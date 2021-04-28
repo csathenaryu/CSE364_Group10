bb64a4ad90052889df7a99ca04c97502b89c5ea0
happyhddey <happyhddey>
2021-04-28 14:35:36 +0900

    refactor : Delete unnecessary classes

--- src/main/java/vimprojector/parsinginputargs/InputArgument.java
+++
@@ -1,7 +0,0 @@
-package vimprojector.parsinginputargs;
-
-import java.util.ArrayList;
-
-public interface InputArgument {
-    void filter(ArrayList<String> mappedArg, String lowerArg);
-}
---
+++ src/main/java/vimprojector/recommender/MovieListPrinter.java
@@ -0,0 +1,12 @@
+package vimprojector.recommender;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MovieListPrinter {
+
+    public void print(ArrayList<Integer> recommendedMovie, ArrayList<HashMap<String, String>> movieData, ArrayList<HashMap<String, String>> linkData){
+
+    }
+
+}

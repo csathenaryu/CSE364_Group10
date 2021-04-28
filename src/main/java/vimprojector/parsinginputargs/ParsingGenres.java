bd8f819eacd48d86dc92b4391ec8b92fe7b0e423
ahnjongmin <79144650+ahnjongmin@users.noreply.github.com>
2021-04-28 21:02:21 +0900

    feat: Change Genrespool(adventure added)

--- src/main/java/vimprojector/parsinginputargs/ParsingGenres.java
+++ src/main/java/vimprojector/parsinginputargs/ParsingGenres.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 public class ParsingGenres extends ParsingInputArgument{
     static Pair propertyMapping = new Pair(
-            "action:action,animation:animation,children's:children's,comedy:comedy,crime:crime,documentary:documentary,drama:drama,fantasy:fantasy,film-noir:film-noir,horror:horror,musical:musical,mystery:mystery,romance:romance,scifi:scifi,thriller:thriller,war:war,western:western");
+            "action:action,adventure:adventure,animation:animation,children's:children's,comedy:comedy,crime:crime,documentary:documentary,drama:drama,fantasy:fantasy,film-noir:film-noir,horror:horror,musical:musical,mystery:mystery,romance:romance,scifi:scifi,thriller:thriller,war:war,western:western");
 
     public String[] parseProperty(String inputProp, String delimiter){
         return getParsedProperty(propertyMapping, inputProp, delimiter);

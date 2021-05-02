51e7e88f24ea8055f6ddc7fbcfbc5b26afb02ef5
syparksy98 <64393959+20171113@users.noreply.github.com>
2021-05-03 08:07:54 +0900

    test : Add RatingCounterTest

--- src/test/java/vimprojector/recommender/RatingCounterTest.java
+++ src/test/java/vimprojector/recommender/RatingCounterTest.java
@@ -1,5 +1,23 @@
 package vimprojector.recommender;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class RatingCounterTest {
 
+    @Test
+    public void getAverageRating_normal() {
+        RatingCounter ratingCounter = new RatingCounter();
+        ratingCounter.update(4);
+        ratingCounter.update(5);
+        ratingCounter.update(6);
+        Assert.assertEquals(ratingCounter.getAverageRating(), 5, 0.001);
+
+    }
+    @Test
+    public void getAverageRating_empty() {
+        RatingCounter ratingCounter = new RatingCounter();
+        Assert.assertEquals(ratingCounter.getAverageRating(), 0, 0.001);
+
+    }
 }

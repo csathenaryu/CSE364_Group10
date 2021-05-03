270db9b7b69c69464011a13af258a5842dd984e6
ahnjongmin <79144650+ahnjongmin@users.noreply.github.com>
2021-05-03 19:11:39 +0900

    docs: Change run.sh to Milestone2

--- run.sh
+++ run.sh
@@ -6,9 +6,10 @@ folder="CSE364_Group10"
 git clone $repository
 
 cd $folder
-git checkout main
+git checkout develop_milestone2
 
 mvn install
 mvn assembly:assembly
 
-java -cp target/CSE364-project-1.0-SNAPSHOT-jar-with-dependencies.jar main.java.Main "adventure|romance" educator
+java -cp target/CSE364-project-1.0-SNAPSHOT-jar-with-dependencies.jar main.java.Milestone2 "" "" ""
+java -cp target/CSE364-project-1.0-SNAPSHOT-jar-with-dependencies.jar main.java.Milestone2 "" "" "" "Adventure"

7f65aad2377e6304a596a9ac021c2933eaa48435
ahnjongmin <79144650+ahnjongmin@users.noreply.github.com>
2021-06-03 23:20:36 +0900

    feat: MongoDB

--- src/main/java/group10/CSE364project/MoviedataRepository.java
+++ src/main/java/group10/CSE364project/MoviedataRepository.java
@@ -1,4 +1,4 @@
-package group10.CSE364project;
+/*package group10.CSE364project;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,3 +8,18 @@ interface MoviedataRepository extends JpaRepository<MovieData, String> {
 
 }
 
+*/
+
+package group10.CSE364project;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "movies", path = "movies")
+public interface MoviedataRepository extends MongoRepository<MovieData, String> {
+
+
+}
